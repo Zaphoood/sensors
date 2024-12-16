ad96d389f498b5ba88d8b0523e96e511655adc34
Mathis Weber <mathis.c.weber@web.de>
2024-12-16 15:12:53 +0100

    draw coordinate axis labels

--- circumcircle3.py
+++ circumcircle3.py
@@ -186,13 +186,26 @@ class Node:
 
 
 class CoordinateAxes:
-    def __init__(self, color: Color) -> None:
+    def __init__(
+        self, color: Color, text_offset: Tuple[int, int] = (0, -15), font_size: int = 16
+    ) -> None:
         self.color = color
+        self.text_offset = np.array(text_offset)
+        self.font = pygame.font.SysFont("Courier", font_size)
 
     def draw(self, screen: pygame.Surface, camera: Camera) -> None:
         origin = np.array([0, 0, 0])
-        for endpoint in [[1, 0, 0], [0, 1, 0], [0, 0, 1]]:
-            draw_line3d(screen, camera, self.color, origin, np.array(endpoint))
+        for endpoint, label in zip([[1, 0, 0], [0, 1, 0], [0, 0, 1]], ["x", "y", "z"]):
+            endpoint3d = np.array(endpoint)
+
+            draw_line3d(screen, camera, self.color, origin, endpoint3d)
+
+            endpoint2d = camera.world_to_screen(cast(Vector, endpoint3d * 1.1))
+            if endpoint2d is not None:
+                text_position = tuple(
+                    np.round(endpoint2d + self.text_offset).astype(int)
+                )
+                screen.blit(self.font.render(label, True, self.color), text_position)
 
 
 def draw_line3d(
