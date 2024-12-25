b8e6c234d3b38a2645d507b031747ea815324a79
Mathis Weber <mathis.c.weber@web.de>
2024-12-25 23:49:36 +0100

    test visibility of edges instead of vertices

--- plane_sweep.py
+++ plane_sweep.py
@@ -1,11 +1,13 @@
+import logging
+import time
 from typing import List, Optional, cast
 
 import numpy as np
+
 from delaunay import sort_triangle
 from util import Triangle, Vector, random_scatter_sphere, shift
-import logging
 
-logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+logging.basicConfig(format="%(message)s", level=logging.WARN)
 
 
 def plane_sweep(
@@ -28,77 +30,80 @@ def plane_sweep(
         logging.info("--- iteration ---")
         logging.debug(f"{vertex=} {points[vertex]}")
         logging.debug(f"boundary: {boundary_vertices}")
-        # Boundary vertices that are visible from current vertex
-        visible_boundary_vertices = np.full((len(boundary_vertices)), True)
+        if points[vertex][1] < 0:
+            logging.info("crossed equator, stop.")
+            return triangulation
+        # Edges of the boundary that are visible from current vertex.
+        # Entry i corresponds to edge (boundary_vertices[i], boundary_vertices[(i + 1) % len(boundary_vertices)])
+        visible_boundary_edges = np.full((len(boundary_vertices)), True)
 
-        special_case_hidden_edge = None
-        for i in range(len(boundary_vertices)):
-            for b0 in range(len(boundary_vertices)):
-                b1 = (b0 + 1) % len(boundary_vertices)
-                visible_boundary_vertices[i] &= not do_arcs_intersect(
-                    points[vertex],
-                    points[boundary_vertices[i]],
-                    points[boundary_vertices[b0]],
-                    points[boundary_vertices[b1]],
+        for i, (current_idx, next_idx) in enumerate(
+            zip(boundary_vertices, shift(boundary_vertices))
+        ):
+            midpoint = (points[current_idx] + points[next_idx]) / 2
+            midpoint /= np.linalg.norm(midpoint)
+            for b0, b1 in zip(boundary_vertices, shift(boundary_vertices)):
+                if (current_idx, next_idx) == (b0, b1):
+                    continue
+                visible_boundary_edges[i] &= not (
+                    do_arcs_intersect(
+                        points[vertex],
+                        points[current_idx],
+                        points[b0],
+                        points[b1],
+                    )
+                    or do_arcs_intersect(
+                        points[vertex],
+                        cast(Vector, midpoint),
+                        points[b0],
+                        points[b1],
+                    )
+                    or do_arcs_intersect(
+                        points[vertex],
+                        points[next_idx],
+                        points[b0],
+                        points[b1],
+                    )
                 )
 
-        for current in range(len(boundary_vertices)):
-            next = (current + 1) % len(boundary_vertices)
-
-            if visible_boundary_vertices[current] and visible_boundary_vertices[next]:
-                if len(boundary_vertices) == 3 and special_case_hidden_edge is None:
-                    third = (next + 1) % len(boundary_vertices)
-
-                    point_current = points[boundary_vertices[current]]
-                    point_next = points[boundary_vertices[next]]
-                    point_third = points[boundary_vertices[third]]
-                    midpoint = cast(Vector, (point_current + point_next) / 2)
-
-                    if do_arcs_intersect(
-                        points[vertex], midpoint, point_next, point_third
-                    ) or do_arcs_intersect(
-                        points[vertex], midpoint, point_third, point_current
-                    ):
-                        special_case_hidden_edge = [
-                            boundary_vertices[current],
-                            boundary_vertices[next],
-                        ]
-                        continue
-
-                new_triangle = sort_triangle(
-                    (vertex, boundary_vertices[current], boundary_vertices[next])
-                )
+        logging.debug(f"{visible_boundary_edges=}")
+        for i, (current_idx, next_idx) in enumerate(
+            zip(boundary_vertices, shift(boundary_vertices))
+        ):
+            if visible_boundary_edges[i]:
+                new_triangle = sort_triangle((vertex, current_idx, next_idx))
                 logging.info(new_triangle)
                 triangulation.append(new_triangle)
 
         # Modify boundary to include the current vertex and remove those that are no longer boundary vertices
         # The latter are vertices which are visible and have visible neighbours.
-        # The array `visible_boundary_vertices` will look something like this:
+        # The array `visible_boundary_edges` will look something like this:
         #   [F ... F T... T F ... F] (case i.)
         # or this:
         #   [T ... T F ... F T ... T] (case ii.)
         # or this:
         #   [T T T] (special case iii.)
-        # The index of T after F is called `t_start`, the index of T before F `t_end`
-        # We want to remove all indices after `t_start` and before `t_end`
+        #
+        # Store the index of the first visible vertex of the sequence as
+        # `t_start` and the last as `t_end`; we want to remove vertices at all
+        # indices after `t_start` and before `t_end`
         t_start: Optional[int] = None
         t_end: Optional[int] = None
-        for current in range(len(visible_boundary_vertices)):
-            next = (current + 1) % len(visible_boundary_vertices)
+        for current_idx in range(len(visible_boundary_edges)):
+            next_idx = (current_idx + 1) % len(visible_boundary_edges)
             if (
-                not visible_boundary_vertices[current]
-                and visible_boundary_vertices[next]
+                not visible_boundary_edges[current_idx]
+                and visible_boundary_edges[next_idx]
             ):
-                t_start = next
+                t_start = next_idx
             if (
-                visible_boundary_vertices[current]
-                and not visible_boundary_vertices[next]
+                visible_boundary_edges[current_idx]
+                and not visible_boundary_edges[next_idx]
             ):
-                t_end = current
+                t_end = next_idx
 
         if t_start is None and t_end is None:
-            assert special_case_hidden_edge is not None
-            boundary_vertices = [*special_case_hidden_edge, vertex]
+            raise RuntimeError("All vertices visible or all invisible :(")
         else:
             assert t_start is not None and t_end is not None
             if t_start < t_end:  # case i.
@@ -111,8 +116,8 @@ def plane_sweep(
                 boundary_vertices = [*boundary_vertices[t_end : t_start + 1], vertex]
 
     # Fill in final hole with last vertex
-    for current, next in zip(boundary_vertices, shift(boundary_vertices)):
-        triangulation.append((points_sorted[-1], current, next))
+    for current_idx, next_idx in zip(boundary_vertices, shift(boundary_vertices)):
+        triangulation.append((points_sorted[-1], current_idx, next_idx))
 
     return triangulation
 
@@ -163,26 +168,27 @@ def _is_in_arc(v: Vector, a: Vector, b: Vector) -> bool:
 def _test_with_seed(seed: int, n_points: int) -> None:
     np.random.seed(seed)
     points = random_scatter_sphere(n_points)
-    points = [cast(Vector, point * [1, -1, 1]) for point in points]
 
     triangulation = plane_sweep(points)
     print(triangulation)
 
 
 def main():
-    # n_repeat = 10
-    # n_points = 20
-    # for _ in range(n_repeat):
-    #     seed = int(time.time() * 1000) % 2**32
+    n_repeat = 1000
+    n_points = 10
+    for i in range(n_repeat):
+        print(i, end="\r")
+        seed = int(time.time() * 1000) % 2**32
+        np.random.seed(seed)
+        points = random_scatter_sphere(n_points)
 
-    #     print(f"{seed=}")
-    #     np.random.seed(seed)
-    #     points = random_scatter_sphere(n_points)
+        try:
+            plane_sweep(points)
+        except AssertionError:
+            print(seed)
+            break
 
-    #     triangulation = plane_sweep(points)
-    #     print(triangulation)
-
-    _test_with_seed(4288346723, n_points=20)
+    # _test_with_seed(4288346723, n_points=20)
 
 
 if __name__ == "__main__":
